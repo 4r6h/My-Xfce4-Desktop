7cd951086b2b8cd14ccb973415d945bb3f1a75a7
4r6h <108066623+4r6h@users.noreply.github.com>
2022-07-19 15:54:28 +0600

    Update InstallParu.sh

--- InstallParu.sh
+++ InstallParu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo pacman -S --noconfirm --needed base base-devel;git clone https://aur.archlinux.org/paru.git;cd paru;makepkg -si
+sudo pacman -S --noconfirm --needed base base-devel;git clone https://aur.archlinux.org/paru.git;cd paru;makepkg -si --noconfirm
 echo "Paru Installed"
 rm -rf paru
