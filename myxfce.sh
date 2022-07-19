d2dee2eeae886bdc650bacd9eac409233bea5de8
4r6h <108066623+4r6h@users.noreply.github.com>
2022-07-19 17:40:28 +0600

    Update myxfce.sh

--- myxfce.sh
+++ myxfce.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 mkdir $HOME/.config/xfce4/
-git clone https://github.com/4r6h/myxfce.git;mv -u myxfce/* $HOME/.config/xfce4/
+git clone https://github.com/4r6h/myxfce.git
+wait
+mv -u myxfce/* $HOME/.config/xfce4/
