f5b7d6125bbe6badbcbef2d15a737f04334be2a5
4r6h <108066623+4r6h@users.noreply.github.com>
2022-07-19 17:19:36 +0600

    Update set-sddm.sh

--- set-sddm.sh
+++ set-sddm.sh
@@ -6,4 +6,4 @@ wget https://raw.githubusercontent.com/4r6h/mysddm/main/sddm.conf
 wait
 wget https://raw.githubusercontent.com/4r6h/mysddm/main/theme.conf
 wait
-sudo mv -u sddm.conf /etc/sddm.conf && sudo mv -u theme.conf /usr/share/sddm/themes/sugar-candy/theme.conf
+sudo mv -u sddm.conf /etc/sddm.conf && sudo mv -u theme.conf /usr/share/sddm/themes/Sugar-Candy/theme.conf
