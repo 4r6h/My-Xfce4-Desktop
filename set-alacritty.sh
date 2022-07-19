b2d98f6792b6a5c4584af3191a20f5a1ad6020e1
4r6h <108066623+4r6h@users.noreply.github.com>
2022-07-19 16:29:34 +0600

    Update set-alacritty.sh

--- set-alacritty.sh
+++ set-alacritty.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+mkdir $HOME/.config/alacritty/
 paru -S --noconfirm alacritty;wget https://raw.githubusercontent.com/4r6h/AlacrittyConfig/main/alacritty.yml;wget https://raw.githubusercontent.com/dracula/alacritty/master/dracula.yml;mv -u alacritty.yml $HOME/.config/alacritty/alacritty.yml && mv -u dracula.yml $HOME/.config/alacritty/dracula.yml
