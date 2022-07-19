3f58db3e900e8cce81cdf4e7679169a010a7b620
4r6h <hrahat023@gmail.com>
2022-07-19 15:24:22 +0600

    Added 3 Files

---
+++ set-alacritty.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+paru -S --noconfirm alacritty;wget https://raw.githubusercontent.com/4r6h/AlacrittyConfig/main/alacritty.yml;wget https://raw.githubusercontent.com/dracula/alacritty/master/dracula.yml;mv -u alacritty.yml $HOME/.config/alacritty/alacritty.yml && mv -u dracula.yml $HOME/.config/alacritty/dracula.yml
---
+++ set-rofi.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wget https://raw.githubusercontent.com/4r6h/myrofi/main/rofi/config.rasi;mv -u config.rasi $HOME/.config/rofi/
