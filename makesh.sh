2b13f5ec224dd61db46bea47f30bd95e2435b9ec
4r6h <108066623+4r6h@users.noreply.github.com>
2022-07-26 00:50:06 +0600

    Delete makesh.sh

--- makesh.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/env bash
-chmod +x 01-InstallAll.sh 02-Fix-display-resolution-in-Arch-Linux-VM.sh InstallFonts.sh InstallParu.sh lightdm-fix.sh myxfce.sh set-alacritty.sh set-rofi.sh set-sddm.sh
