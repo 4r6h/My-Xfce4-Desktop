c93db2b729b5cd39c391515ae306f58125220d32
4r6h <hrahat023@gmail.com>
2022-08-06 18:55:55 +0000

    Added some functions

--- 01-install-script.sh
+++ 01-install-script.sh
@@ -5,22 +5,22 @@ do
 read -p "Enter your username: " username
 
 update_mirrors="
-pacman --needed --noconfirm -S wget vim reflector
+pacman --noconfirm --needed -S wget vim reflector
 wget -c https://raw.githubusercontent.com/4r6h/Dot4iles/main/set-locale.sh;chmod +x set-locale.sh;./set-locale.sh
 wget -c https://raw.githubusercontent.com/4r6h/ArchyMirrorsBD/main/updatemirrors.sh;chmod +x updatemirrors.sh;./updatemirrors.sh
 "
 vmmachine="
-sudo pacman --needed --noconfirm -S virtualbox-guest-utils
+sudo pacman --noconfirm --needed -S virtualbox-guest-utils
 sudo systemctl enable vboxservice.service
 "
 noparu="
 su ${username}
 wget -c https://raw.githubusercontent.com/4r6h/Dot4iles/main/InstallParu.sh;chmod +x InstallParu.sh;./InstallParu.sh
-paru -Syu - <packages.txt --needed --noconfirm
+paru -Syu - <packages.txt --noconfirm --needed 
 "
 yesparu="
 su ${username}
-paru -Syu - <packages.txt --needed --noconfirm
+paru -Syu - <packages.txt --noconfirm --needed
 "
 common="
 wget -c https://raw.githubusercontent.com/4r6h/Dot4iles/main/InstallFonts.sh;chmod +x InstallFonts.sh;./InstallFonts.sh
@@ -32,7 +32,7 @@ wget -c https://raw.githubusercontent.com/4r6h/Dot4iles/main/set-alacritty.sh;ch
 vminstall="
 exec $update_mirrors
 exec $vmmachine
-if [ -f /bin/paru ]; then
+if [ -f /bin/paru* ]; then
 	exec $yesparu
 else 
 	exec $noparu
@@ -42,7 +42,7 @@ exec $common
 
 normalinstall="
 exec $update_mirrors
-if [ -f /bin/paru ]; then
+if [ -f /bin/paru* ]; then
 	exec $yesparu
 else 
 	exec $noparu
@@ -54,6 +54,7 @@ if [ ! -d /home/$username ]
 then
     echo "User does not exist. First create a normal user with home dirrectory and give the user sudo or wheel privilages"
 else
+cd /home/$username; mkdir My_Xfce4_Desktop_Scripts; cd My_Xfce4_Desktop_Scripts
 	read -r -p "Are You Installing in Virtual Machine? then press enter [(Y/n) (defult=Y)] " vm
     case $vm in
 	    [yY][eE][sS]|[yY]|$ENTER)
