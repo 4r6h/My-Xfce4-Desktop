71abdc13c0db77b4fd222161add403e887368fb8
4r6h <hrahat023@gmail.com>
2022-08-12 14:12:25 +0000

    code rearrangement

--- 01-install-script.sh
+++ 01-install-script.sh
@@ -2,7 +2,11 @@
 
 if [ $( whoami ) = "root" ]; then
 	
-	if [ ! -d My_Xfce4_Desktop_Scripts ]; then
+	if [ -d My_Xfce4_Desktop_Scripts ]; then
+
+                rmdir My_Xfce4_Desktop_Scripts
+        
+	elif [ ! -d My_Xfce4_Desktop_Scripts ]; then
 
                 mkdir My_Xfce4_Desktop_Scripts; cd My_Xfce4_Desktop_Scripts
         fi
